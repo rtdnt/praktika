7558375df867c94242074316d0a377b0d44ea166
rtdnt <111079129+rtdnt@users.noreply.github.com>
2022-10-29 22:10:11 +0300

    Task 1: comments in js file, html and css changes

--- JS atsiskaitymas/Task 1/script.js
+++ JS atsiskaitymas/Task 1/script.js
@@ -8,15 +8,15 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
-document.querySelector("form").addEventListener("submit", (e) => { 
+document.querySelector("form").addEventListener("submit", (e) => { // When the form is submitted, the function is executed
 
-    e.preventDefault(); 
+    e.preventDefault(); // this prevents the reload of the page
 
-    let weightInKilograms = document.getElementById("search").value;
+    let weightInKilograms = document.getElementById("search").value; // value is assigned to the number customer enters
 
-    document.getElementById("output").style.visibility = "visible";
+    document.getElementById("output").style.visibility = "visible"; // opens the div tag (below) with style
     
-    document.getElementById("svarai").innerHTML = weightInKilograms * 2.2046;
-    document.getElementById("gramai").innerHTML = weightInKilograms / 0.0010000;
-    document.getElementById("uncijos").innerHTML = weightInKilograms * 35.274;
+    document.getElementById("svarai").innerHTML = weightInKilograms * 2.2046; // calculates and assigns value in pounds
+    document.getElementById("gramai").innerHTML = weightInKilograms / 0.0010000; // shows the value in kilograms 
+    document.getElementById("uncijos").innerHTML = weightInKilograms * 35.274; // calculates and assigns the value in ounces
 });
