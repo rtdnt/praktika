774ec1b6549aeac22ea8d88dc3f927f216b5dbc3
rtdnt <111079129+rtdnt@users.noreply.github.com>
2022-10-30 13:03:03 +0200

    Task 10 completed

--- JS atsiskaitymas/Task 10/script.js
+++ JS atsiskaitymas/Task 10/script.js
@@ -2,6 +2,10 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
 
+import { composition, division, substraction, multiplication } from "./modules/math/index.js";
+import { one, two, three, four, five } from "./modules/numbers/numbers.js";
+
+
 let a = composition(one, four);
 let b = division(four, two);
 let c = substraction(three, two);
